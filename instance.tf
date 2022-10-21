09b837716f2a2999597d1bcc610f0e0805844154
il4real <ijantikov@gmail.com>
2022-10-21 13:37:35 +0300

    formatting docs

--- instance.tf
+++ instance.tf
@@ -1,12 +1,13 @@
 resource "aws_instance" "blue" {
-    ami = "${var.AMI}"
-    instance_type = "t2.micro"
-    subnet_id = "${aws_subnet.bg-subnet-pub-1.id}"
-    vpc_security_group_ids = ["${aws_security_group.ssh-allowed.id}"]
-    user_data = "${file("nginx.sh")}"
+  count                  = 2
+  ami                    = var.AMI
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.bg-subnet-pub-1.id
+  vpc_security_group_ids = ["${aws_security_group.ssh-allowed.id}"]
+  user_data              = file("nginx.sh")
 }
 
 output "security_check" {
-    description = "check_sg"
-    value = aws_security_group.ssh-allowed.id
+  description = "check_sg"
+  value       = aws_security_group.ssh-allowed.id
 }
--- vpc.tf
+++ vpc.tf
@@ -3,7 +3,7 @@ resource "aws_vpc" "bluegreen" {
   enable_dns_support   = true
   enable_dns_hostnames = true
   instance_tenancy     = "default"
-  
+
 
   tags = {
     Name = "bluegreen"
